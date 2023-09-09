fa09f33788e150b3a705bf15b9d832e3a78ae651
Mohammad <reza.qasimi2000@gmail.com>
2023-09-09 09:06:21 +0430

    Testimonials section added and styled

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ hlink.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   faSolid.classList.toggle("fa-xmark");
 });
+
+// Testimonials
+const userTexts = document.querySelectorAll(".user-text");
+const userPics = document.querySelectorAll(".user-pic");
+
+function showReview() {
+  // remover the pic borders
+  for (userPic of userPics) {
+    userPic.classList.remove("active-pic");
+  }
+  // remove the text
+  for (userText of userTexts) {
+    userText.classList.remove("active-text");
+  }
+
+  let i = Array.from(userPics).indexOf(event.target);
+  userPics[i].classList.add("active-pic");
+  userTexts[i].classList.add("active-text");
+}
