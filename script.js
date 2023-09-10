85be15156a07e1a69caa15e4b79c224fbd16297f
Mohammad <reza.qasimi2000@gmail.com>
2023-09-10 11:08:43 +0430

    header became sticky

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// const sectionn = document.querySelectorAll('section');
+// const navlinkss = document.querySelectorAll('nav ul li a');
+const navbar = document.querySelector("header");
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("bg-color-primary-dark");
+    navbar.classList.add("border-b");
+    navbar.classList.add("border-color-gray");
+  } else {
+    navbar.classList.remove("bg-color-primary-dark");
+    navbar.classList.remove("border-b");
+    navbar.classList.remove("border-color-gray");
+  }
+};
+// Mobile Menu
 const humburger = document.querySelector("#humburger");
 const menu = document.querySelector("#menu");
 const hlink = document.querySelector("#hlink");
